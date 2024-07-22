78ddee7d6bccdee6b8c63f6c8f10bf46e12475de
Rafaelmmt <144057595+Rafaelmmt@users.noreply.github.com>
2024-07-22 11:29:09 -0300

    Aula 86

--- 5. Curso 05/Note App/src/views/ViewNotes.vue
+++ 5. Curso 05/Note App/src/views/ViewNotes.vue
@@ -3,26 +3,31 @@
     <div class="has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Escreva aqui sua nota..." />
+          <textarea 
+            class="textarea" 
+            v-model="newNote" 
+            placeholder="Escreva aqui sua nota..."
+            ref="newNoteRef" 
+          />
         </div>
       </div>
-    
+
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success-ligth">Add New Note</button>
+          <button
+            class="button is-link has-background-success-ligth"
+            @click="addNote"
+            :disabled="!newNote"
+          >
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
-    
-    <div class="card mb-4" v-for="i in 3">
+
+    <div class="card mb-4" v-for="note in notes" :key="note.id">
       <div class="card-content">
-        <div class="content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem nibh, maximus commodo sollicitudin eu, volutpat et mi. 
-            Vestibulum rhoncus velit id turpis eleifend, dictum placerat purus lobortis. Nam vitae mauris in augue tristique tincidunt id sit amet neque. 
-            Curabitur gravida diam nec ullamcorper sollicitudin. Phasellus vitae dui id dolor vulputate scelerisque. Praesent porta semper dui. Donec nec pretium mi. 
-            Aenean tristique lacus eu sodales sodales.
-          </p>
-        </div>
+        <div class="content">{{ note.content }}</div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -30,5 +35,32 @@
       </footer>
     </div>
   </div>
-
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const newNote = ref('')
+const newNoteRef = ref(null)
+const notes = ref([
+  {
+    id: 'id1',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem nibh, maximus commodo sollicitudin eu, volutpat et mi. Vestibulum rhoncus velit id turpis eleifend, dictum placerat purus lobortis.'
+  },
+  {
+    id: 'id2',
+    content: 'Lorem ipsum da turpis eleifend, dictum placerat purus lobortis.'
+  }
+])
+const addNote = () => {
+  let currentDate = new Date().getTime()
+  let id = currentDate.toString()
+  let note = {
+    id: id,
+    content: newNote.value
+  }
+  notes.value.unshift(note)
+  newNote.value = ''
+  newNoteRef.value.focus()
+}
+</script>
